83f6adc00088abd129094500ee9e696f7e0d68ea
Branimir Lapcevic <112483983+BrandoL87@users.noreply.github.com>
2023-05-06 15:59:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿
+
+using Skupovi;
+
+namespace SkupoviAplikacija
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+/*
+            Skup a = new Skup();
+            a.niz = new int[] { 3, 5, 6, 7, 9, 10 };
+
+            Skup b = new Skup();
+            b.niz = new int[] { 8, 9, 10, 11 };
+
+            Skup c = a.Presek(b);
+            Skup d = a.Unija(b);
+            Skup e = a.Razlika(b);
+
+            foreach(int i in c.niz)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach(var i in d.niz)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach(var i in e.niz)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(c.ToString());
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(e.ToString());
+
+            Console.WriteLine();
+
+            //Skup n = new Skup();
+           // n.ucitajSkup();
+
+            
+*/
+            Console.WriteLine("**** Dobrodosli u aplikaciju Skupovi *** \n");
+
+
+            while (true)
+            {
+                Console.WriteLine("\n Unesite odgovarajucu opciju.");
+                Console.WriteLine("Unesite '1' za presek skupova.");
+                Console.WriteLine("Unesite '2' za uniju skupova .");
+                Console.WriteLine("Unesite '3' za razliku skupova.");
+                Console.WriteLine("Unesite '4' za duzinu skupova.");
+                Console.WriteLine("Unesite '.' za kraj.");
+               
+
+                //Korisnik bira operaciju koju zeli da izvrsava
+                string? opcija = Console.ReadLine();
+
+                //Korisnik je izabrao da napusti aplikaciju
+                if (opcija == ".")
+                    break;
+
+                
+
+                switch (opcija)
+                {
+                    case("1"): {
+                            //unosimo elemente dva skupa i odredjujemo njihov presek
+                            
+                            Console.WriteLine("Unesite elemente skupa A:");
+                            Skup A = new Skup();
+                            A.ucitajSkup();
+                            Console.WriteLine("Unesite elemente skupa B:");
+                            Skup B = new Skup();
+                            B.ucitajSkup();
+                            Console.WriteLine($"A = {A.ToString()}");
+                            Console.WriteLine($"B = {B.ToString()}");
+                            Console.WriteLine($"Presek skupova je: {A.Presek(B).ToString()}");
+
+                            break;
+                        }
+                    case ("2"):
+                        {
+                            //unosimo elemente dva skupa i odredjujemo njihovu uniju
+                            
+                            Console.WriteLine("Unesite elemente skupa A:");
+                            Skup A = new Skup();
+                            A.ucitajSkup();
+                            Console.WriteLine("Unesite elemente skupa B:");
+                            Skup B = new Skup();
+                            B.ucitajSkup();
+                            Console.WriteLine($"A = {A.ToString()}");
+                            Console.WriteLine($"B = {B.ToString()}");
+                            Console.WriteLine($"Unija skupova je: {A.Unija(B).ToString()}");
+
+                            break;
+                        }
+
+                    case ("3"):
+                        {
+                            //unosimo elemente dva skupa i odredjujemo njihovu razliku
+                            
+                            Console.WriteLine("Unesite elemente skupa A:");
+                            Skup A = new Skup();
+                            A.ucitajSkup();
+                            Console.WriteLine("Unesite elemente skupa B:");
+                            Skup B = new Skup();
+                            B.ucitajSkup();
+                            Console.WriteLine($"A = {A.ToString()}");
+                            Console.WriteLine($"B = {B.ToString()}");
+                            Console.WriteLine($" Razlika skupova je: {A.Razlika(B).ToString()}");
+
+                            break;
+                        }
+                    case ("4"):
+                        {
+                            //odredjujemo broj elemenata skupa
+                           
+                            Console.WriteLine("Unesite elemente skupa A:");
+                            Skup A = new Skup();
+                            A.ucitajSkup();
+                            Console.WriteLine("Unesite elemente skupa B:");
+                            Skup B = new Skup();
+                            B.ucitajSkup();
+                            Console.WriteLine($"Duzina skupa A je: {A.duzinaSkupa()}");
+                            Console.WriteLine($"Duzina skupa B je: {B.duzinaSkupa()}");
+                            break;
+                        }
+
+                    default:
+                        {
+                            //Korisnik nije ispravno uneo opciju iz menija
+                            Console.WriteLine("\n Niste uneli odgovarajucu opciju!!!");
+                            break;
+                        }
+
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Skup.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skupovi
+{
+    public class Skup
+    {
+       //niz je tipa int? da bi omogucili i null vrednosti, jer bi u
+       //suprotnom podrayumevane vrednosti na preostalim mestima u nizu bile 0.
+        public int?[] niz = new int?[100] ;
+
+        //prazan konstruktor
+        public Skup()
+        {
+
+        }
+        //konstruktor sa parametrom 
+        public Skup(int?[] niz)
+        {
+            this.niz = niz;
+        }
+        //ucitavanje elemenata skupa preko ulaza, sve dok korisnik 
+        //ne unese neki drugi karakter
+        public void ucitajSkup()
+        {
+            
+            Console.WriteLine($"Za prekid unosa unesite bilo koji karakter.");
+            int m;
+            int i = 0;
+            bool uslov = true;
+
+            while(uslov)
+            {
+               string? s = Console.ReadLine();
+                bool condition = int.TryParse(s, out m);
+
+                if(condition)
+                {
+                    niz[i] = m;
+                    i++;
+                }
+                else
+                {
+                    uslov = false;
+                }
+            }
+
+            this.niz = niz.Distinct().ToArray() ;
+        }
+        
+
+        
+        //implementacija preseka skupova preko LINQ-a
+        //biramo samo one elemente koji se javljaju u oba skupa i redjamo ih po velicini
+        //koristimo konstruktor sa parametrom za izlaznu vrednost
+        public Skup Presek(Skup B)
+        {
+            return new Skup(niz.Where(i => B.niz.Contains(i)).OrderBy(i => i).ToArray());
+        }
+        //unija dva skupa, elementi su poredjani po velicini
+        public Skup Unija(Skup B) {
+            return new Skup(niz.Union(B.niz).OrderBy(i => i).ToArray());
+        }
+        //razlika dva skupa
+        public Skup Razlika(Skup B)
+        {
+            return  new Skup(niz.Where(i => !B.niz.Contains(i)).OrderBy(i => i).ToArray());
+        }
+        //ispis rezultujuceg skupa u odgovarajucem formatu, koristeci matematicki zapis 
+        //za skupove
+        public override string ToString()
+        {
+            string s = "{";
+            foreach (var i in this.niz)
+            {
+                if(i!= null)
+                s += i + ",";
+            }
+
+            s = s.Remove(s.Length - 1);
+            s += "}";
+            return s;
+        }
+        //duzinu skupa odredjujemo prebrojavajuci ne-null vrednosti niza
+        public int duzinaSkupa()
+        {
+            return niz.Count(i => i!= null);
+        }
+    }
+}
